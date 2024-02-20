ec374324e26d1de97bda7f56b00a40b2fa644600
Ilija Pecevski <pecevec13@gmail.com>
2024-02-20 19:25:04 +0100

    Relative path for json file added

--- Data_Driven_Approach/Data/AnimalContext.cs
+++ Data_Driven_Approach/Data/AnimalContext.cs
@@ -1,7 +1,9 @@
 ﻿using Data_Driven_Approach.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -29,15 +31,24 @@ namespace Data_Driven_Approach.Data
 
             string jsonString = JsonSerializer.Serialize(_animals);
 
-            string filePath = @"C:\Users\Ilija\source\repos\OldMacDonaldsHadFarm\Data_Driven_Approach\animals.json";
+            string relativePath = @"animals.json";
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectRoot = Directory.GetParent(baseDirectory).Parent.Parent.Parent.FullName;
+            string filePath = Path.Combine(projectRoot, relativePath);
+           
+            //string filePath = @"C:\Users\Ilija\source\repos\OldMacDonaldsHadFarm\Data_Driven_Approach\animals.json";
             File.WriteAllText(filePath, jsonString);
             _ioProvider.PrintToOutput("Animal is saved to JSON document."); 
         }
 
         private List<Animal> GetAllAnimals()
         {
+            string relativePath = @"animals.json";
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectRoot = Directory.GetParent(baseDirectory).Parent.Parent.Parent.FullName;
+            string path = Path.Combine(projectRoot, relativePath);
 
-            string path = @"C:\Users\Ilija\source\repos\OldMacDonaldsHadFarm\Data_Driven_Approach\animals.json";
+            //string path = @"C:\Users\Ilija\source\repos\OldMacDonaldsHadFarm\Data_Driven_Approach\animals.json";
             try
             {
                 if (File.Exists(path))
